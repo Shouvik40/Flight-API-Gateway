7898a0f9bf5a94ebb300cc69cf58a70ff5ace9c4
Shouvik40 <105535909+Shouvik40@users.noreply.github.com>
2023-08-22 22:21:30 +0530

    Added middlewares for auth

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { UserController } = require("../../controllers");
+const { AuthRequestMiddlewares } = require("../../middlewares");
 
 const router = express.Router();
 // const { AirplaneMiddlewares } = require("../../middlewares");
 
 // /api/v1/user/signup POST
-router.post("/signup", UserController.signup);
-router.post("/signin", UserController.signin);
+router.post("/signup", AuthRequestMiddlewares.validateAuthRequest, UserController.signup);
+router.post("/signin", AuthRequestMiddlewares.validateAuthRequest, UserController.signin);
 
 module.exports = router;
