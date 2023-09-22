cbf8a98a869f807db0a51a0eb8e87f0ad4546cfb
Shouvik40 <105535909+Shouvik40@users.noreply.github.com>
2023-09-22 19:35:31 +0530

    Added default customer role while signup

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -10,7 +10,7 @@ const { SuccessResponse, ErrorResponse } = require("../utils/common");
  */
 async function signup(req, res) {
   try {
-    const user = await UserService.signup({
+    const user = await UserService.create({
       email: req.body.email,
       password: req.body.password,
     });
